d57220bd651a1b128a5db96ba5b7a2a26a6d7186
Davi Asaf <148470155+DaviAsaf@users.noreply.github.com>
2024-08-28 10:24:18 -0300

    Update Vetor-28-09.js

--- Vetor-28-09.js
+++ Vetor-28-09.js
@@ -1,5 +1,4 @@
-Primeiro Exercicio
-<script>
+//Primeiro Exercicio
         var vetor = []
         var tamanho = 10
         for(i=0;i<tamanho;i++){
@@ -11,9 +10,8 @@ Primeiro Exercicio
             }
         }
         alert(`${vetor[0]}`)
-    </script>
-Segundo Exercicio
-var vetor = []
+//Segundo Exercicio
+        var vetor = []
         var pares = []
         var tamanho = 10
         for (i = 0; i < tamanho; i++) {
@@ -26,3 +24,27 @@ var vetor = []
         }
         alert(`Os números pares são ${pares}`)
         alert("Os números pares são: "+ pares)
+//Terceiro Exercicio
+        estudante = []
+        total = parseInt(prompt("Quantos estudantes vão ser cadastrados? "))
+        for(i=0;i<total;i++){
+            estudante[i] = prompt(`Estudantes ${i+1}: `)
+        }
+        for(i=0;i<total;i++){
+            estudante[i] = " " + estudante[i]
+        }
+        estudante.sort()
+        alert(estudante)
+//Quarto Exercicio
+        var numeroaleatorio = parseInt(Math.random()*100)
+        var respostausuario = prompt("Tente adivinhar um número (Entre 1 a 100)")
+        while(numeroaleatorio != respostausuario){
+            let dica_maior = respostausuario > numeroaleatorio
+            if(dica_maior){
+                respostausuario = prompt(`${respostausuario} é maior que o número sorteado`)
+            }
+            else{
+                respostausuario = prompt(`${respostausuario} é menor que o número sorteado`)
+            }
+        }
+        alert("Parabens, você encontrou o número sorteado: "+ numeroaleatorio)
